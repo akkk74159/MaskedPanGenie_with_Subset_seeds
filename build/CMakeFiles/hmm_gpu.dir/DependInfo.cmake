7a520e0d044b12bc68ae53e7f15edb3ebf4bcc32
akkk74159 <45625640+akkk74159@users.noreply.github.com>
2025-09-24 16:55:44 +0800

    Add files via upload

---
+++ build/CMakeFiles/hmm_gpu.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CUDA"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/home/eric/thesis/MaskedPangenie/src/hmm_gpu.cu" "/home/eric/thesis/MaskedPangenie/build/CMakeFiles/hmm_gpu.dir/src/hmm_gpu.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CUDA
+  "USE_CUDA"
+  )
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  "/home/eric/anaconda3/envs/pangenie/include/jellyfish-2.2.10"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hmm_gpu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hmm_gpu.dir/src/hmm_gpu.cu.o"
+  "libhmm_gpu.a"
+  "libhmm_gpu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/hmm_gpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
